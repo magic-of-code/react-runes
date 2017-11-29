dca67c25f2e13dac2412ef11d07d349dc3e1305a
Merlin Patterson <merlinpatt@gmail.com>
2017-11-29 15:58:47 -0500

    Create Message component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Message = () => {
+  return (
+    <div>
+      Some message here.
+    </div>
+  );
+};
+
 class App extends Component {
   render() {
     return (
@@ -10,9 +18,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Message />
       </div>
     );
   }
